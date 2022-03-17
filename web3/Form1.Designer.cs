f53a1f5a38e4f7ca2be71b1119e362cef8cfc2e7
Kseniya Makarova <ksenija.mackarova2014@yandex.ru>
2022-03-17 21:17:31 +0300

    Добавьте файлы проекта.

---
+++ web3/Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿
+namespace web3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton4 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.toolStripButton5 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton6 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton9 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.закладкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.историяToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.удалитьИзЗакладокToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripButton10 = new System.Windows.Forms.ToolStripButton();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.deleteHistoryMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(800, 450);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripButton2,
+            this.toolStripButton3,
+            this.toolStripButton4,
+            this.toolStripTextBox1,
+            this.toolStripButton5,
+            this.toolStripButton7,
+            this.toolStripButton8,
+            this.toolStripButton6,
+            this.toolStripButton9,
+            this.toolStripButton10});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 33);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton2.Text = "toolStripButton2";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // toolStripButton3
+            // 
+            this.toolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton3.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton3.Image")));
+            this.toolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton3.Name = "toolStripButton3";
+            this.toolStripButton3.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton3.Text = "toolStripButton3";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
+            // 
+            // toolStripButton4
+            // 
+            this.toolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton4.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton4.Image")));
+            this.toolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton4.Name = "toolStripButton4";
+            this.toolStripButton4.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton4.Text = "toolStripButton4";
+            this.toolStripButton4.Click += new System.EventHandler(this.toolStripButton4_Click);
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(400, 33);
+            // 
+            // toolStripButton5
+            // 
+            this.toolStripButton5.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton5.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton5.Image")));
+            this.toolStripButton5.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton5.Name = "toolStripButton5";
+            this.toolStripButton5.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton5.Text = "toolStripButton5";
+            this.toolStripButton5.Click += new System.EventHandler(this.toolStripButton5_Click);
+            // 
+            // toolStripButton7
+            // 
+            this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton7.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton7.Image")));
+            this.toolStripButton7.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton7.Name = "toolStripButton7";
+            this.toolStripButton7.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton7.Text = "toolStripButton7";
+            this.toolStripButton7.Click += new System.EventHandler(this.toolStripButton7_Click);
+            // 
+            // toolStripButton8
+            // 
+            this.toolStripButton8.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton8.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
+            this.toolStripButton8.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton8.Name = "toolStripButton8";
+            this.toolStripButton8.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton8.Text = "toolStripButton8";
+            this.toolStripButton8.Click += new System.EventHandler(this.toolStripButton8_Click);
+            // 
+            // toolStripButton6
+            // 
+            this.toolStripButton6.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton6.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton6.Image")));
+            this.toolStripButton6.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton6.Name = "toolStripButton6";
+            this.toolStripButton6.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton6.Text = "toolStripButton6";
+            this.toolStripButton6.Click += new System.EventHandler(this.toolStripButton6_Click);
+            // 
+            // toolStripButton9
+            // 
+            this.toolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton9.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.закладкиToolStripMenuItem,
+            this.историяToolStripMenuItem,
+            this.удалитьИзЗакладокToolStripMenuItem});
+            this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton9.Image")));
+            this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton9.Name = "toolStripButton9";
+            this.toolStripButton9.Size = new System.Drawing.Size(42, 28);
+            this.toolStripButton9.Text = "toolStripButton9";
+            // 
+            // закладкиToolStripMenuItem
+            // 
+            this.закладкиToolStripMenuItem.Name = "закладкиToolStripMenuItem";
+            this.закладкиToolStripMenuItem.Size = new System.Drawing.Size(280, 34);
+            this.закладкиToolStripMenuItem.Text = "Закладки";
+            // 
+            // историяToolStripMenuItem
+            // 
+            this.историяToolStripMenuItem.Name = "историяToolStripMenuItem";
+            this.историяToolStripMenuItem.Size = new System.Drawing.Size(280, 34);
+            this.историяToolStripMenuItem.Text = "История";
+            this.историяToolStripMenuItem.Click += new System.EventHandler(this.историяToolStripMenuItem_Click);
+            // 
+            // удалитьИзЗакладокToolStripMenuItem
+            // 
+            this.удалитьИзЗакладокToolStripMenuItem.Name = "удалитьИзЗакладокToolStripMenuItem";
+            this.удалитьИзЗакладокToolStripMenuItem.Size = new System.Drawing.Size(280, 34);
+            this.удалитьИзЗакладокToolStripMenuItem.Text = "Удалить из закладок";
+            this.удалитьИзЗакладокToolStripMenuItem.Click += new System.EventHandler(this.удалитьИзЗакладокToolStripMenuItem_Click);
+            // 
+            // toolStripButton10
+            // 
+            this.toolStripButton10.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton10.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton10.Image")));
+            this.toolStripButton10.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton10.Name = "toolStripButton10";
+            this.toolStripButton10.Size = new System.Drawing.Size(34, 28);
+            this.toolStripButton10.Text = "toolStripButton10";
+            this.toolStripButton10.Click += new System.EventHandler(this.toolStripButton10_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 48);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(755, 26);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(747, 0);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Вкладка 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(747, 0);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Вкладка 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // deleteHistoryMenu
+            // 
+            this.deleteHistoryMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.deleteHistoryMenu.Name = "deleteHistoryMenu";
+            this.deleteHistoryMenu.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.ToolStripButton toolStripButton3;
+        private System.Windows.Forms.ToolStripButton toolStripButton4;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripButton toolStripButton5;
+        private System.Windows.Forms.ToolStripButton toolStripButton7;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripButton toolStripButton8;
+        private System.Windows.Forms.ContextMenuStrip deleteHistoryMenu;
+        private System.Windows.Forms.ToolStripButton toolStripButton10;
+        private System.Windows.Forms.ToolStripButton toolStripButton6;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripButton9;
+        private System.Windows.Forms.ToolStripMenuItem закладкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem историяToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem удалитьИзЗакладокToolStripMenuItem;
+    }
+}
+
---
+++ web3/Form1.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace web3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            historyBox.Text = File.ReadAllText("history.txt");
+            bookmarks.Items.AddRange(File.ReadAllLines("bookmarks.txt"));
+        }
+
+        ListBox bookmarks = new ListBox();
+        RichTextBox historyBox = new RichTextBox();
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+
+        }
+
+        private void toolStripTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && toolStripTextBox1.Text != null && tabControl1.SelectedTab.Text != "История" && tabControl1.SelectedTab.Text != "Закладки")
+            {
+                webBrowser1.Navigate(toolStripTextBox1.Text);
+                File.AppendAllText("history.txt", toolStripTextBox1.Text + "\n");
+                historyBox.Text = File.ReadAllText("history.txt");
+            }
+            else { }
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+            if (toolStripTextBox1.Text != null && tabControl1.SelectedTab.Text != "История" && tabControl1.SelectedTab.Text != "Закладки")
+            {
+                webBrowser1.Navigate(toolStripTextBox1.Text);
+                File.AppendAllText("history.txt", toolStripTextBox1.Text + "\n");
+                historyBox.Text = File.ReadAllText("history.txt");
+            }
+            else { }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab.Text != "История" && tabControl1.SelectedTab.Text != "Закладки")
+            {
+                webBrowser1.Refresh();
+            }
+            else { }
+        }
+
+        private void toolStripButton5_Click(object sender, EventArgs e)
+        {
+            if (toolStripTextBox1.Text != null && tabControl1.SelectedTab.Text != "История" && tabControl1.SelectedTab.Text != "Закладки")
+            {
+                webBrowser1.Navigate(toolStripTextBox1.Text);
+                File.AppendAllText("history.txt", toolStripTextBox1.Text + "\n");
+                historyBox.Text = File.ReadAllText("history.txt");
+            }
+            else { }
+        }
+
+        private void toolStripButton4_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedTab.Text != "Новая вкладка" && tabControl1.SelectedTab.Text != "История" && tabControl1.SelectedTab.Text != "Закладки")
+            {
+                webBrowser1.Stop();
+            }
+        }
+
+        int tabCounter = 0;
+
+        private void Browser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            tabControl1.SelectedTab.Text = ((WebBrowser)tabControl1.SelectedTab.Controls[0]).DocumentTitle;
+        }
+
+        private void toolStripButton7_Click(object sender, EventArgs e)
+        {
+            WebBrowser browser = new WebBrowser();
+            browser.Visible = true;
+            browser.ScriptErrorsSuppressed = true;
+            browser.Dock = DockStyle.Fill;
+            browser.DocumentCompleted += Browser_DocumentCompleted;
+            tabControl1.TabPages.Add("Новая вкладка");
+            tabControl1.SelectTab(tabCounter+1);
+            tabControl1.SelectedTab.Controls.Add(browser);
+            tabCounter++;
+        }
+
+        private void toolStripButton8_Click(object sender, EventArgs e)
+        {
+            if (tabControl1.TabPages.Count > 1)
+            {
+                tabControl1.TabPages.RemoveAt(tabControl1.SelectedIndex);
+                tabControl1.SelectTab(tabControl1.TabPages.Count - 1);
+                tabCounter--;
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void toolStripButton6_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText("bookmarks.txt", tabControl1.SelectedTab.Text + "\n");
+            bookmarks.Items.Add(tabControl1.SelectedTab.Text + "\n");
+        }
+
+        private void toolStripButton10_Click(object sender, EventArgs e)
+        {
+             webBrowser1.GoHome();
+        }
+
+        private void историяToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Add("История");
+            tabControl1.SelectTab(tabCounter);
+            tabControl1.SelectedTab.Controls.Add(historyBox);
+            historyBox.Dock = DockStyle.Fill;
+            tabCounter++;
+        }
+
+        private void закладкиToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            tabControl1.TabPages.Add("Закладки");
+            tabControl1.SelectTab(tabCounter);
+            tabControl1.SelectedTab.Controls.Add(bookmarks);
+            bookmarks.Dock = DockStyle.Fill;
+            bookmarks.ContextMenuStrip = deleteHistoryMenu;
+            tabCounter++;
+        }
+
+        private void удалитьИзЗакладокToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+                {
+                 List<string> bookmarksContent = new List<string>();
+                 bookmarksContent.AddRange(File.ReadAllLines("bookmarks.txt"));
+                 bookmarksContent.RemoveAt(bookmarks.SelectedIndex);
+                 File.WriteAllLines("bookmarks.txt", bookmarksContent);
+                 bookmarks.Items.RemoveAt(bookmarks.SelectedIndex);
+                }
+            catch { }
+        }
+
+    }
+}
